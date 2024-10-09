dff44d3c23b6928725a5d147146b9bc16255ccbf
AlessioMartella7 <alessio.mart87@gmail.com>
2024-10-09 17:55:02 +0200

    refactored spacing

--- index.php
+++ index.php
@@ -20,6 +20,7 @@ include_once __DIR__.'/db/db.php';
         <div class="container">
             <div class="row my-5 bg-dark p-4">
                 <?php foreach($movies as $movie) { ?>
+
                 <div class="col-4">
                     <div class="card border-warning mb-3" style="max-width: 25rem;">
                         <div class="card-header card-title">
@@ -54,22 +55,4 @@ include_once __DIR__.'/db/db.php';
     </main>
 </body>
 
-
-
 </html>
-
-<!-- è definita una classe ‘Movie’
-=> all'interno della classe sono dichiarate delle variabili d'istanza
-=> all'interno della classe è definito un costruttore
-=> all'interno della classe è definito almeno un metodo
-vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
-Bonus 1:
-Modificare la classe Movie in modo che accetti piú di un genere.
-Bonus 2:
-Creare un layout completo per stampare a schermo una lista di movies.
-Facciamo attenzione all’organizzazione del codice, suddividendolo in appositi file e cartelle.
-Possiamo ad esempio organizzare il codice
-creando un file dedicato ai dati (tipo gli array di oggetti) che potremmo chiamare db.php
-mettendo ciascuna classe nel proprio file e magari raggruppare tutte le classi in una cartella dedicata che possiamo chiamare classes/
-organizzando il layout dividendo la struttura ed i contenuti in file e parziali dedicati.
-Buon lavoro e buon divertimento a tutte e a tutti! -->
